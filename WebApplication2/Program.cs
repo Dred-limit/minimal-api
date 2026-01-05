0506ce73b3d36aaf04b1f0c7d7fc7098d56f0a2f
Dred-limit <53877236+Dred-limit@users.noreply.github.com>
2026-01-06 01:03:53 +0300

    the file structure has been corrected

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -4,21 +4,31 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var _fruit = new ConcurrentDictionary<string, Fruit>();
-app.MapGet("/fruit", () => _fruit);
 
+
+app.MapGet("/fruit", () => _fruit);
 
 app.MapGet("/fruit/{id}", (string id) =>
     _fruit.TryGetValue(id, out var fruit)
     ? TypedResults.Ok(fruit)
-    : Results.Problem(statusCode: 404));
+    : Results.Problem(statusCode: 404))
+    .AddEndpointFilter<IdValidationFilter>();
 
-app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
+// именованный маршрут и LinkGenerator
+app.MapGet("/fruit/{id}", (string id) => $"The details is {id}")
+   .WithName("GetFruitById");
+
+
+app.MapPost("/fruit/{id}", (string id, Fruit fruit, LinkGenerator links) =>
     _fruit.TryAdd(id, fruit)
-    ? TypedResults.Created($"/fruit/{id}", fruit)
+    ? TypedResults.Created( 
+        links.GetPathByName("GetFruitById", new { id }), fruit)
     : Results.ValidationProblem(new Dictionary<string, string[]>
     {
         { "id", new[] {"A fruit with this id already exists"}}
     }));
+
+//? TypedResults.Created($"/fruit/{id}", fruit)
 
 
 app.MapPut("/fruit/{id}", (string id, Fruit fruit) =>
@@ -34,3 +44,29 @@ app.MapDelete("/fruit/{id}", (string id) =>
 });
 app.Run();
 record Fruit(string Name, int Stock);
+
+class IdValidationFilter : IEndpointFilter
+{
+    public async ValueTask<object?> InvokeAsync(
+        EndpointFilterInvocationContext context,
+        EndpointFilterDelegate next)
+    {
+        var id = context.GetArgument<string>(0);
+        if (string.IsNullOrEmpty(id) || !id.StartsWith('f'))
+        {
+            return Results.ValidationProblem(
+                new Dictionary<string, string[]>
+                {
+                    {"id", new[]{"Invalid format. Id must start with 'f"}}
+                });
+        }
+        return await next(context);
+    }
+}
+
+
+
+
+
+
+
